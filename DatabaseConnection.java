86c690a0732b06eeccf9e37a58d66166eb4618a2
Rexonis <140373571+Rexonis@users.noreply.github.com>
2024-05-23 08:21:28 +0500

    Add files via upload

---
+++ DatabaseConnection.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/score";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "admin";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+}
